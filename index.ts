75687b33baf9ffe60cd2fdd5ba6b588378d49a7c
Eduardo Brito <edu.santos.brito@gmail.com>
2020-07-02 22:58:55 -0300

    Migrating create-nrd-app to typescript

--- index.ts
+++ index.ts
@@ -1,6 +1,50 @@
-import { getPackageVersions, packages } from './src/functions';
-import { RequiredPackageItem } from './src/declarations';
+// tslint:disable-next-line:no-var-requires
+require('module-alias/register');
+import { Command } from 'commander';
+import chalk from 'chalk';
+import { version } from './package.json';
+import {
+    generateFolder,
+    generatePackageJson,
+    generateDockerCompose,
+    generateTsConfigJson,
+    generateServerPackageJson,
+    generateServerIndex,
+    generateEslintConfig,
+} from '@app/functions';
+import inquirer from 'inquirer';
+import { spinnies } from '@app/utis';
 
-packages.forEach((packageItem: RequiredPackageItem) => {
-    console.log(getPackageVersions(packageItem.name));
-});
+const program = new Command();
+program.version(version);
+console.log(`\nWelcome to ${chalk.magenta('create-nrd-app')}\n`);
+program
+    .arguments('<projectName>')
+    .option('-n, --useNpm', 'Use npm to install dependencies')
+    .action(async (projectName: string, { useNpm }: { useNpm: Boolean }) => {
+        console.log(`Starting ${chalk.cyan('create-nrd-app')}...`);
+        console.log(`Resolving ${chalk.cyan('server')}...`);
+        console.log({ useNpm });
+        await generateFolder(projectName);
+        await generateFolder(`${projectName}/server`);
+        await generateFolder(`${projectName}/server/src`);
+        await generatePackageJson(projectName);
+        const { dockerComposeFilesInJson }: { dockerComposeFilesInJson: Boolean } = await inquirer.prompt({
+            type: 'list',
+            message: 'Choose file type for docker-compose:',
+            name: 'dockerComposeFilesInJson',
+            choices: [
+                { name: 'JSON', value: true },
+                { name: 'YML', value: false },
+            ],
+        });
+        await generateDockerCompose(projectName, 'production', dockerComposeFilesInJson);
+        await generateDockerCompose(projectName, 'development', dockerComposeFilesInJson);
+        await generateTsConfigJson(projectName);
+        await generateServerPackageJson(projectName);
+        spinnies.add('create-server-package-json', { text: 'Creating server/package.json' });
+        spinnies.succeed('create-server-package-json', { text: 'server/package.json created!' });
+        await generateServerIndex(projectName);
+        await generateEslintConfig(projectName);
+    });
+program.parse(process.argv);
--- src/functions.ts
+++ src/functions.ts
@@ -1,18 +1,240 @@
-import { RequiredPackageItem, PackageInfo } from './declarations';
+import { PackageInfo, PackageVersion, PackageInfoParsed, RequiredPackages } from '@app/types';
 import fetch from 'node-fetch';
+import { lt } from 'semver';
+import fs, { promises as fsPromise } from 'fs';
+import { spinnies } from './utis';
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+import { SpinnerOptions } from 'spinnies';
+import jsonToYaml from 'json-to-pretty-yaml';
+import { renderTemplateFile } from 'template-file';
+import latestVersion from 'latest-version';
 
-const packages: RequiredPackageItem[] = [{ name: 'tiny-tarball' }];
+const serverPackages: RequiredPackages = {
+    dependencies: [{ name: 'body-parser' }, { name: 'cors' }, { name: 'express' }, { name: 'compression' }],
+    devDependencies: [
+        { name: '@types/compression' },
+        { name: '@types/express' },
+        { name: '@types/jest' },
+        { name: '@types/cors' },
+        { name: '@types/node' },
+        { name: '@types/supertest' },
+        { name: '@typescript-eslint/eslint-plugin' },
+        { name: '@typescript-eslint/parser' },
+        { name: 'eslint' },
+        { name: 'eslint-config-airbnb-base' },
+        { name: 'eslint-config-prettier' },
+        { name: 'eslint-plugin-import' },
+        { name: 'eslint-plugin-prettier' },
+        { name: 'husky' },
+        { name: 'jest' },
+        { name: 'lint-staged' },
+        { name: 'nodemon' },
+        { name: 'prettier' },
+        { name: 'supertest' },
+        { name: 'ts-jest' },
+        { name: 'ts-node' },
+        { name: 'typescript' },
+    ],
+};
 
-async function getPackageVersions(packageName: string): Promise<PackageInfo> {
+function getLastPackageVersions(versions: PackageVersion[]): string[] {
+    return Object.keys(versions)
+        .filter((version) => /^([0-9]+)\.([0-9]+)\.([0-9]+)$/.test(version))
+        .sort((a, b) => (lt(a, b) ? 1 : -1))
+        .slice(0, 20);
+}
+
+async function getPackageVersions(packageName: string): Promise<PackageInfoParsed> {
     const response = await fetch(`https://registry.npmjs.org/${packageName}`, {
         headers: {
             Accept: 'application/vnd.npm.install-v1+json',
         },
     });
 
-    const data: PackageInfo = await response.json();
+    const { versions }: PackageInfo = await response.json();
 
-    return data;
+    return {
+        versions: getLastPackageVersions(versions),
+    };
 }
 
-export { packages, getPackageVersions };
+async function generateFolder(folderPath: string): Promise<SpinnerOptions> {
+    spinnies.add('generate-folder', { text: `Creating ${folderPath}` });
+    if (fs.existsSync(`${process.cwd()}/${folderPath}`)) {
+        spinnies.fail('generate-folder', { text: 'Folder already exist, use another name! Aborting...' });
+        return process.exit(1);
+    }
+    await fsPromise.mkdir(`${process.cwd()}/${folderPath}`);
+    return spinnies.succeed('generate-folder', { text: `Folder ${folderPath} created!` });
+}
+
+async function generatePackageJson(projectName: string): Promise<SpinnerOptions> {
+    const values = await inquirer.prompt([
+        {
+            name: 'name',
+            message: 'What is the project name?',
+            default: projectName,
+        },
+        {
+            name: 'version',
+            message: 'What is the project version?',
+            default: '0.1.0',
+        },
+        {
+            name: 'description',
+            message: `Describe your project here ${chalk.yellow('(optional)')}:`,
+            default: null,
+        },
+        {
+            name: 'author',
+            message: `What is the project author? ${chalk.yellow('(optional)')}`,
+            default: null,
+        },
+        {
+            name: 'license',
+            message: 'What is the project license?',
+            default: 'ISC',
+        },
+    ]);
+    spinnies.add('create-package-json', { text: 'Creating package.json' });
+    fsPromise.writeFile(`${process.cwd()}/${projectName}/package.json`, JSON.stringify(values, null, 2));
+    return spinnies.succeed('create-package-json', { text: 'package.json created!' });
+}
+
+async function getDockerComposeData(projectName: string, environment: string, isProduction: Boolean): Promise<string> {
+    return renderTemplateFile(`${process.cwd()}/templates/docker-compose.template.mustache`, {
+        projectName,
+        environment,
+        dockerfile: isProduction ? 'Dockerfile' : 'dev.Dockerfile',
+        appPort: isProduction ? 80 : 3000,
+        appContainerPort: isProduction ? '3000:80' : '3000:3000',
+    });
+}
+
+async function generateDockerCompose(projectName: string, environment: string, dockerComposeFilesInJson: Boolean): Promise<SpinnerOptions> {
+    const isProduction = !(environment === 'development');
+
+    spinnies.add(`create-${environment}-docker`, {
+        text: `Creating docker-compose${!isProduction ? '.dev' : ''}${dockerComposeFilesInJson ? '.json' : '.yml'}`,
+    });
+
+    const fileData = await getDockerComposeData(projectName, environment, isProduction);
+
+    await fsPromise.writeFile(
+        `${process.cwd()}/${projectName}/docker-compose${!isProduction ? '.dev' : ''}${dockerComposeFilesInJson ? '.json' : '.yml'}`,
+        dockerComposeFilesInJson ? fileData : jsonToYaml.stringify(JSON.parse(fileData)),
+    );
+    return spinnies.succeed(`create-${environment}-docker`, {
+        text: `docker-compose${!isProduction ? '.dev' : ''}${dockerComposeFilesInJson ? '.json' : '.yml'} created!`,
+    });
+}
+
+async function getTsConfigData(): Promise<string> {
+    return renderTemplateFile(`${process.cwd()}/templates/tsconfig.template.mustache`);
+}
+
+async function generateTsConfigJson(projectName: string): Promise<SpinnerOptions> {
+    spinnies.add('create-ts-config', { text: 'Creating server/tsconfig.json' });
+    await fs.promises.writeFile(`${process.cwd()}/${projectName}/server/tsconfig.json`, await getTsConfigData());
+    return spinnies.succeed('create-ts-config', { text: 'server/tsconfig.json created!' });
+}
+
+async function getDependenciesVersions() {
+    const { wantToSelectVersions }: { wantToSelectVersions: Boolean } = await inquirer.prompt({
+        type: 'list',
+        message: 'Do you want to select the dependencies versions? (Selecting no, the last version will be selected)',
+        name: 'wantToSelectVersions',
+        choices: [
+            { name: 'Yes', value: true },
+            { name: 'No', value: false },
+        ],
+    });
+    if (!wantToSelectVersions) {
+        return serverPackages.dependencies.reduce(async (promiseAccumulator, dependency) => {
+            const accumulator = await promiseAccumulator;
+            return { ...accumulator, [dependency.name]: await latestVersion(dependency.name) };
+        }, Promise.resolve({}));
+    }
+    return serverPackages.dependencies.reduce(async (promiseAccumulator, dependency) => {
+        const accumulator = await promiseAccumulator;
+        const { selectedVersion }: { selectedVersion: string } = await inquirer.prompt({
+            type: 'list',
+            message: `Select version for ${chalk.green(dependency.name)}`,
+            name: 'selectedVersion',
+            choices: (await getPackageVersions(dependency.name)).versions.map((version) => ({ name: version, value: version })),
+        });
+        return { ...accumulator, [dependency.name]: selectedVersion };
+    }, Promise.resolve({}));
+}
+
+async function getDevDependenciesVersions() {
+    const { wantToSelectVersions }: { wantToSelectVersions: Boolean } = await inquirer.prompt({
+        type: 'list',
+        message: 'Do you want to select the devDependencies versions? (Selecting no, the last version will be selected)',
+        name: 'wantToSelectVersions',
+        choices: [
+            { name: 'Yes', value: true },
+            { name: 'No', value: false },
+        ],
+    });
+    if (!wantToSelectVersions) {
+        return serverPackages.devDependencies.reduce(async (promiseAccumulator, dependency) => {
+            const accumulator = await promiseAccumulator;
+            return { ...accumulator, [dependency.name]: await latestVersion(dependency.name) };
+        }, Promise.resolve({}));
+    }
+    return serverPackages.devDependencies.reduce(async (promiseAccumulator, dependency) => {
+        const accumulator = await promiseAccumulator;
+        const { selectedVersion }: { selectedVersion: string } = await inquirer.prompt({
+            type: 'list',
+            message: `Select version for ${chalk.green(dependency.name)}`,
+            name: 'selectedVersion',
+            choices: (await getPackageVersions(dependency.name)).versions.map((version) => ({ name: version, value: version })),
+        });
+        return { ...accumulator, [dependency.name]: selectedVersion };
+    }, Promise.resolve({}));
+}
+
+async function getServerPackageJsonData(projectName: string): Promise<string> {
+    return renderTemplateFile(`${process.cwd()}/templates/api.packageJson.template.mustache`, {
+        projectName,
+        dependencies: JSON.stringify(await getDependenciesVersions(), null, 2),
+        devDependencies: JSON.stringify(await getDevDependenciesVersions(), null, 2),
+    });
+}
+
+async function generateServerPackageJson(projectName: string): Promise<void> {
+    return fs.promises.writeFile(`${process.cwd()}/${projectName}/server/package.json`, await getServerPackageJsonData(projectName));
+}
+
+async function getServerIndexExpress(): Promise<string> {
+    return renderTemplateFile(`${process.cwd()}/templates/api.index.template.mustache`);
+}
+
+async function generateServerIndex(projectName: string): Promise<SpinnerOptions> {
+    spinnies.add('create-index-server', { text: 'Creating server/src/server.ts' });
+    await fs.promises.writeFile(`${process.cwd()}/${projectName}/server/src/server.ts`, await getServerIndexExpress());
+    return spinnies.succeed('create-index-server', { text: 'server/src/server.ts created!' });
+}
+
+async function getEslintConfigData(): Promise<string> {
+    return renderTemplateFile(`${process.cwd()}/templates/api.eslint.template.mustache`)
+}
+
+async function generateEslintConfig(projectName: string): Promise<SpinnerOptions> {
+    spinnies.add('create-eslint-config', { text: 'Creating server/.eslintrc.json' });
+    await fs.promises.writeFile(`${process.cwd()}/${projectName}/server/.eslintrc.json`, await getEslintConfigData());
+    return spinnies.succeed('create-eslint-config', { text: 'server/.eslintrc.json created!' });
+}
+
+export {
+    getPackageVersions,
+    generateFolder,
+    generatePackageJson,
+    generateDockerCompose,
+    generateTsConfigJson,
+    generateServerIndex,
+    generateServerPackageJson,
+    generateEslintConfig
+};
